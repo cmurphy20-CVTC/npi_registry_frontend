9062d70b4c22c030b29ea06ed8d4a17c603b558d
Casey <cmurphy20@student.cvtc.edu>
2023-05-06 19:51:40 -0500

    axois library used to handle large json requests

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,6 @@
 const express = require("express");
-const https = require("https");
 const bodyParser = require("body-parser");
-const fetch = require("node-fetch");
+const axios = require("axios");
 const ejs = require("ejs");
 
 
@@ -28,47 +27,27 @@ app.get("/no_results", function(req, res) {
 
 app.post("/", function(req, res){
     
+  const npiNumber = req.body.npiNumber;
+  const first_name = req.body.firstName;
+  const last_name = req.body.lastName;
+  const taxonomy_description = req.body.taxonomyDesc;
+  const city = req.body.cityName;
+  const state = req.body.state;
+  const zip_code = req.body.zipCode;
 
-    const npiNumber = req.body.npiNumber;
-    // const npiNumber = "1245319599";
-    // const npiNumber = "";
-    const first_name = req.body.firstName;
-    const last_name = req.body.lastName;
-    const taxonomy_description = req.body.taxonomyDesc;
-    const city = req.body.cityName;
-    const state = req.body.state;
-    const zip_code = req.body.zipCode;
-    // const limit = "50";
-    // const maxLimit = "1200";
+  const url = "https://npiregistry.cms.hhs.gov/api/?number=" + npiNumber + "&enumeration_type=&taxonomy_description=" + taxonomy_description + "&name_purpose=&first_name=" + first_name + "&last_name=" + last_name + "&organization_name=&city=" + city + "&state=" + state +"&postal_code=" + zip_code + "&country_code=US&limit=200&skip=&pretty=&version=2.1";
 
-    const url = "https://npiregistry.cms.hhs.gov/api/?number=" + npiNumber + "&enumeration_type=&taxonomy_description=" + taxonomy_description + "&name_purpose=&first_name=" + first_name + "&last_name=" + last_name + "&organization_name=&city=" + city + "&state=" + state +"&postal_code=" + zip_code + "&country_code=US&limit=&skip=&pretty=&version=2.1"
-    
-   
-   try {
-    https.get(url, function(response){
-      console.log(response.statusCode);
+  axios.get(url).then((response) => {
 
-      response.on("data", function(data){
-          const npiData = JSON.parse(data)
-         
-          console.log(npiData.result_count)
+    const npiData = response.data;
 
-          if (npiData.result_count === 0) {
-            
-            res.redirect("no_results")
-          } else {
-           res.render("results", {resultData: npiData});
-        }
+    res.render("results", {resultData: npiData});
 
-        
+  }).catch((error) => {
+
+    console.log(error)
+
   })
-
- 
-})
-
-} catch (err) {
-  console.log('error', err);
-}
 
 })
 
