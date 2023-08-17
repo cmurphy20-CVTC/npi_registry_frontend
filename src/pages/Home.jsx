a4b988a685af1c4a2c212a92dd72da620b118cc0
Casey <89794911+cmurphy20-CVTC@users.noreply.github.com>
2023-08-16 22:26:14 -0500

    styling added to form

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -55,74 +55,64 @@ function Home() {
   return (
     <div>
       <Nav />
+
       <div className=" ">
         <form
-          className="mx-auto mb-8 w-1/2 bg-white pt-4 shadow-lg"
+          className="mx-auto mb-20 w-1/2 bg-white pt-4 shadow-lg"
           onSubmit={handleSubmit}
         >
-          <div className="mx-auto mb-6 flex  flex-row ">
-            <div className="mx-auto w-1/2 ">
-              <label
-                className="ml-12 flex flex-row text-xl"
-                htmlFor="npiNumber"
-              >
+          <div className="mx-auto mb-6 grid grid-cols-2 gap-4">
+            <div className=" ml-6 w-full">
+              <label className="flex mb-2 flex-row text-xl" htmlFor="npiNumber">
                 NPI Number
               </label>
               <input
                 type="text"
-                className="form-input ml-12 w-3/4 rounded-lg border-gray-300 px-3 py-2 shadow-sm focus:border-blue-500 focus:outline-none focus:ring-1 focus:ring-blue-500"
+                className="form-input mb-6  w-5/6 rounded-lg border-gray-300 px-3 py-2 shadow-sm focus:border-blue-500 focus:outline-none focus:ring-1 focus:ring-blue-500"
                 id="npiNumber"
                 name="npiNumber"
                 onChange={handleChange}
                 value={data.npiNumber}
               ></input>
-            </div>
 
-            <div className="mx-auto w-1/2">
               <label
-                className="ml-16 flex flex-row text-xl"
-                htmlFor="taxonomyDesc"
-              >
-                Taxonomy Description
-              </label>
-              <input
-                type="text"
-                className="form-input ml-16 w-3/4 rounded-lg border-gray-300 px-3 py-2 shadow-sm focus:border-blue-500 focus:outline-none focus:ring-1 focus:ring-blue-500"
-                id="taxonomyDesc"
-                name="taxonomyDesc"
-                onChange={handleChange}
-                value={data.taxonomyDesc}
-              ></input>
-            </div>
-          </div>
-
-          <div className="mx-auto mb-6 flex  flex-row">
-            <div className="mx-auto  w-1/2 ">
-              <label
-                className="ml-12 flex flex-row text-xl"
+                className=" flex flex-row  mb-2 text-xl"
                 htmlFor="firstName"
               >
                 First Name
               </label>
               <input
                 type="text"
-                className="form-input ml-12 w-3/4 rounded-lg border-gray-300 px-3 py-2 shadow-sm focus:border-blue-500 focus:outline-none focus:ring-1 focus:ring-blue-500"
+                className="form-input w-5/6 rounded-lg border-gray-300 px-3 py-2 shadow-sm focus:border-blue-500 focus:outline-none focus:ring-1 focus:ring-blue-500"
                 id="firstName"
                 name="firstName"
                 onChange={handleChange}
                 value={data.firstName}
               ></input>
             </div>
-            <div className="mx-auto  w-1/2">
+
+            <div className="mx-auto w-full ">
+              <label className="flex flex-row text-xl mb-2" htmlFor="taxonomyDesc">
+                Taxonomy Description
+              </label>
+              <input
+                type="text"
+                className="form-input mb-6 w-5/6 rounded-lg border-gray-300 px-3 py-2 shadow-sm focus:border-blue-500 focus:outline-none focus:ring-1 focus:ring-blue-500"
+                id="taxonomyDesc"
+                name="taxonomyDesc"
+                onChange={handleChange}
+                value={data.taxonomyDesc}
+              ></input>
+
               <label
-                className="ml-16 flex flex-row  text-xl"
+                className=" flex flex-row  text-xl mb-2"
                 htmlFor="lastName"
               >
                 Last Name
               </label>
               <input
                 type="text"
-                className="form-input ml-16 w-3/4 rounded-lg border-gray-300 px-3 py-2 shadow-sm focus:border-blue-500 focus:outline-none focus:ring-1 focus:ring-blue-500"
+                className="form-input w-5/6 rounded-lg border-gray-300 px-3 py-2 shadow-sm focus:border-blue-500 focus:outline-none focus:ring-1 focus:ring-blue-500"
                 id="lastName"
                 name="lastName"
                 onChange={handleChange}
@@ -131,35 +121,35 @@ function Home() {
             </div>
           </div>
 
-          <div className="mx-auto mb-6 flex  flex-row">
-            <div className="mx-auto  w-1/3 ">
-              <label className="ml-12 flex flex-row  text-xl" htmlFor="city">
+          <div className="mx-auto mb-6 grid grid-cols-3">
+            <div className="mx-auto  w-full ">
+              <label className="ml-6 flex flex-row  text-xl mb-2" htmlFor="city">
                 City
               </label>
               <input
                 type="text"
-                className="form-input ml-12 w-3/4 rounded-lg border-gray-300 px-3 py-2 shadow-sm focus:border-blue-500 focus:outline-none focus:ring-1 focus:ring-blue-500"
+                className="form-input ml-6 w-5/6 rounded-lg border-gray-300 px-3 py-2 shadow-sm focus:border-blue-500 focus:outline-none focus:ring-1 focus:ring-blue-500"
                 id="city"
                 name="city"
                 onChange={handleChange}
                 value={data.city}
               ></input>
             </div>
-            <div className="mx-auto  w-1/3">
-              <label className="flex-row ml-6 text-xl" htmlFor="state">
+            <div className="mx-auto  w-full ">
+              <label className="ml-6 flex flex-row text-xl mb-2" htmlFor="state">
                 State - Use two letters
               </label>
               <input
                 type="text"
-                className="form-input ml-6 w-3/4 rounded-lg border-gray-300 px-3 py-2 shadow-sm focus:border-blue-500 focus:outline-none focus:ring-1 focus:ring-blue-500"
+                className="form-input ml-6 w-5/6 rounded-lg border-gray-300 px-3 py-2 shadow-sm focus:border-blue-500 focus:outline-none focus:ring-1 focus:ring-blue-500"
                 id="state"
                 name="state"
                 onChange={handleChange}
                 value={data.state}
               ></input>
             </div>
-            <div className="mx-auto  w-1/3">
-              <label className="flex-row  text-xl" htmlFor="zipCode">
+            <div className="mx-auto  w-full">
+              <label className=" flex flex-row  text-xl mb-2" htmlFor="zipCode">
                 Zip Code
               </label>
               <input
@@ -175,7 +165,11 @@ function Home() {
 
           <div className=" mx-auto mb-6 flex  flex-row">
             <div className="mx-auto mb-6 flex  flex-row">
-              <button type="submit" className="w-52 shadow-md bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" value="Submit">
+              <button
+                type="submit"
+                className="w-52 rounded bg-blue-500 py-2 px-4 font-bold text-white shadow-md hover:bg-blue-700"
+                value="Submit"
+              >
                 Search
               </button>
             </div>
